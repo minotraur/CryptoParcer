2a4a1a7e4c383cc618f89c30fcc3d6219b229f4e
Vsesamo <Vsesamo@mail.ru>
2021-10-03 15:01:26 +0300

    Перетаскиваемое окно без рамок и с кнопкой выхода

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests as req
 import json
 from tkinter import *
+import tkinter.ttk as ttk
 
 path_for_coinbase = 'CoinBase.json'
 path_for_tradeogre = 'TradeOgre.json'
@@ -33,11 +34,29 @@ def get_price_from_tradeogre():
 
 
 # Окно
+lastClickX = 0
+lastClickY = 0
+
+def SaveLastClickPos(event):
+    global lastClickX, lastClickY
+    lastClickX = event.x
+    lastClickY = event.y
+
+
+def Dragging(event):
+    x, y = event.x - lastClickX + window.winfo_x(), event.y - lastClickY + window.winfo_y()
+    window.geometry("+%s+%s" % (x , y))
+
 window = Tk()
 window.geometry('400x250')
-window.configure(background="#262525")
-lb = Label(text='Hello')
-
-lb.pack(side=TOP)
-
+window.configure(bg="#262525")
+window.overrideredirect(1)
+window.resizable(False,False)
+window.attributes('-topmost', True)
+window.bind('<Button-1>', SaveLastClickPos)
+window.bind('<B1-Motion>', Dragging)
+close = Button(window,text = "Close",command=window.destroy)
+close.grid(padx=359,pady=1)
+#lb = Label(text='Hello')
+#lb.pack(side=TOP)
 window.mainloop()
